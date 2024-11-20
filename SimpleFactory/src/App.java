1b94b107e23afa097f83f16c40120f8342dca594
MPF <373738633@qq.com>
2024-11-20 14:21:17 +0800

    修改了main方法，创建了不使用简单工厂模式的一般方法。 mpf 2024/11/20 14:22:58

--- SimpleFactory/src/App.java
+++ SimpleFactory/src/App.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
+import netscape.javascript.JSObject;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // 简单工厂模式，以运算器为例，通过对不同运算符的支持生成不同的对象。实现一个计算器控制台程序，要求输入两个数和运算符号。
+        int ret = Caculate();
+        System.out.println("结果是:"+ret);
+
+    }
+    // 先看一般的实现方法
+    public static int Caculate() {
+        int numA,numB;
+        System.out.println("请输入第一个操作数:");
+        Scanner scanner = new Scanner(System.in);
+        numA = scanner.nextInt();
+        System.out.println("请输入第二个操作数:");
+        numB = scanner.nextInt();
+
+        System.out.println("请输入操作符");
+        String operate = scanner.next();
+        int ret;
+        switch (operate) {
+            case "+":
+                ret = numA + numB;
+                break;
+            case "-":
+                ret = numA - numB;
+                break;
+            default:
+                ret = 0;
+                break;
+        }
+        return ret;
     }
 }
